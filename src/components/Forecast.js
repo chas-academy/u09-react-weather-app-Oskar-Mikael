6f19a99f79fd14e6f5df0b24dff8676fc6c3038e
Oskar-Mikael <oskar.bostrom@chasacademy.se>
2021-04-09 12:12:03 +0200

    Adjust unix converter for timezone difference, and add variable for responseObj

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,6 +1,8 @@
 import React from 'react';
 const Forecast = (props) => {
 
+    const response = props.responseObj;
+
     // Capitalize the first letter of a string. Useful, because the weather description starts with a lower case.
     const capitalize = (string) => {
         return string.charAt(0).toUpperCase() + string.slice(1);
@@ -14,10 +16,11 @@ const Forecast = (props) => {
     }
 
     //Convert unix numbers into 24 hour format. Useful, because sunrise and sunset data from API is written in unix format
-    const unixConvert = (unix) => {
-        let date = new Date(unix * 1000);
+    const unixConvert = (unix, timezone) => {
+        const offset = new Date().getTimezoneOffset() / 60;
+        const date = new Date((unix + timezone) * 1000);
 
-        let hours = date.getHours();
+        let hours = date.getHours() + offset;
         if (hours.toString().length === 1) {
             hours = '0' + hours
         }
@@ -77,34 +80,34 @@ const Forecast = (props) => {
 
     return (
         <>
-            {props.responseObj.cod === '200' &&
-                <h2 className="text-4xl mt-12">{props.responseObj.city.name} </h2>}
+            {response.cod === '200' &&
+                <h2 className="text-4xl mt-12">{response.city.name} </h2>}
             <div className="mt-10 grid grid-cols-3">
 
-                {props.responseObj.cod === '200' ?
+                {response.cod === '200' ?
 
                     dayIndexes.map((day, index) => (
 
 
                         <div key={index} className="mb-10 bg-gray-400 mx-auto p-10 rounded-md">
                             <h3>{aryDates[day]}</h3>
-                            <p>{capitalize(props.responseObj.list[day].weather[0].description)}.</p>
+                            <p>{capitalize(response.list[day].weather[0].description)}.</p>
 
-                            <p><strong>Temperature: </strong>{props.responseObj.list[day].temp.day} {unit} </p>
+                            <p><strong>Temperature: </strong>{response.list[day].temp.day} {unit} </p>
 
-                            <p><strong>Temperature highest: </strong>{props.responseObj.list[day].temp.max} {unit} </p>
+                            <p><strong>Temperature highest: </strong>{response.list[day].temp.max} {unit} </p>
 
-                            <p><strong>Temperature lowest: </strong>{props.responseObj.list[day].temp.min} {unit} </p>
+                            <p><strong>Temperature lowest: </strong>{response.list[day].temp.min} {unit} </p>
 
-                            <p><strong>Humidity: </strong>{props.responseObj.list[day].humidity}%</p>
+                            <p><strong>Humidity: </strong>{response.list[day].humidity}%</p>
 
-                            <p><strong>Air Pressure: </strong>{props.responseObj.list[day].pressure} hPa</p>
+                            <p><strong>Air Pressure: </strong>{response.list[day].pressure} hPa</p>
 
-                            <p><strong>Wind: </strong>{props.responseObj.list[day].speed} m/s </p>
+                            <p><strong>Wind: </strong>{response.list[day].speed} m/s </p>
 
-                            <p><strong>Sunrise: </strong>{unixConvert(props.responseObj.list[day].sunrise)}</p>
+                            <p><strong>Sunrise: </strong>{unixConvert(response.list[day].sunrise, response.city.timezone)}</p>
 
-                            <p><strong>Sunset: </strong>{unixConvert(props.responseObj.list[day].sunset)}</p>
+                            <p><strong>Sunset: </strong>{unixConvert(response.list[day].sunset, response.city.timezone)}</p>
 
 
                         </div>
