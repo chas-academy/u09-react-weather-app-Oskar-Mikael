0354d0968ee392a6eacab2d55659df8b7061e641
Oskar-Mikael <oskar.bostrom@chasacademy.se>
2021-04-26 13:20:36 +0200

    Remove favicon

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,14 +1,13 @@
-import favicon from '../assets/favicon.png';
 import sun from '../assets/sun-color.png';
 
 function Header() {
     return(
         <header className="bg-gradient-to-r py-4 from-green-500 to-blue-500 flex justify-evenly">
-            <img src={favicon} className="md:w-36 w-16 hidden sm:block" alt="application-logo"/>
+            <img src={sun} alt="sun" className="App-logo sm:w-36 w-16 hidden sm:block"/>
             <h1 className="text-5xl font-bold my-auto text-center">
                 U09 Weather App
             </h1>
-            <img src={sun} alt="sun" className="md:w-36 w-16 hidden sm:block"/>
+            <img src={sun} alt="sun" className="App-logo md:w-36 w-16 hidden sm:block"/>
         </header>
     )
 
