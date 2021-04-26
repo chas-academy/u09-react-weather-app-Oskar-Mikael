f2513e42e30cf1fa0994c92e1b7282e08a0f7c34
Oskar-Mikael <oskar.bostrom@chasacademy.se>
2021-04-26 14:09:48 +0200

    Loading component

---
+++ src/components/LoadingSpinner.js
@@ -0,0 +1,18 @@
+import Loader from "react-loader-spinner";
+
+function LoadingSpinner() {
+  //other logic
+    return (
+        <div className="flex justify-center">
+      <Loader
+        type="Puff"
+        color="#00BFFF"
+        height={100}
+        width={100}
+        timeout={3000} //3 secs
+      />
+      </div>
+    );
+  }
+
+  export default LoadingSpinner;
