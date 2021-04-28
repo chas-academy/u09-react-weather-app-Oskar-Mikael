d935e2fb0d17f7fe81829bd9b354f1e9499df674
Oskar-Mikael <oskar.bostrom@chasacademy.se>
2021-04-28 16:02:05 +0200

    Map location update when searching new location

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -5,6 +5,7 @@ import ReactMapGL from 'react-map-gl';
 import axios from 'axios';
 import loupe from '../assets/loupe.png';
 import Marker from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 import pin from '../assets/location-pin.png';
 
 function Weather() {
@@ -22,17 +23,21 @@ function Weather() {
     const [long, getLong] = useState();
     const [lat, getLat] = useState();
 
-    useEffect(() => {
-        navigator.geolocation.getCurrentPosition(position => {
-            getLong(position.coords.longitude);
-            getLat(position.coords.latitude);
-        })
-    })
+    const [myLong, getMyLong] = useState();
+    const [myLat, getMyLat] = useState();
 
     const [viewport, setViewport] = useState({
         width: '50vw',
         height: '40vh',
         zoom: 12,
+        coords: [lat, long]
+    })
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(position => {
+            getMyLong(position.coords.longitude);
+            getMyLat(position.coords.latitude);
+        })
     })
 
 
@@ -43,9 +48,9 @@ function Weather() {
             .then(response => {
                 setResponseObj(response.data)
                 console.log(response)
-                setError({})
                 getLong(response.data.city.coord.lon)
                 getLat(response.data.city.coord.lat)
+                setError({})
                 setLoading(false);
             })
             .catch(response => {
@@ -54,6 +59,12 @@ function Weather() {
                 setResponseObj({});
                 setLoading(false);
             })
+        setViewport({
+            width: '50vw',
+            height: '40vh',
+            zoom: 12,
+            coords: [lat, long],
+        })
 
         axios.get(`${process.env.REACT_APP_API_URL_HOURLY}units=${unit}&q=${uriEncodedCity}&appid=${process.env.REACT_APP_API_KEY}`)
             .then(response => {
@@ -72,15 +83,13 @@ function Weather() {
 
     const getMyPosition = () => {
         setLoading(true);
-        axios.get(`${process.env.REACT_APP_API_URL}units=${unit}&lat=${lat}&lon=${long}&appid=${process.env.REACT_APP_API_KEY}`)
+        axios.get(`${process.env.REACT_APP_API_URL}units=${unit}&lat=${myLat}&lon=${myLong}&appid=${process.env.REACT_APP_API_KEY}`)
             .then(response => {
                 setResponseObj(response.data);
                 console.log(responseObj);
                 setError({})
-                navigator.geolocation.getCurrentPosition(position => {
-                    getLong(position.coords.longitude);
-                    getLat(position.coords.latitude);
-                })
+                getLat(false)
+                getLong(false)
                 setLoading(false);
             })
             .catch(response => {
@@ -90,7 +99,7 @@ function Weather() {
                 setLoading(false);
             })
 
-        axios.get(`${process.env.REACT_APP_API_URL_HOURLY}units=${unit}&lat=${lat}&lon=${long}&appid=${process.env.REACT_APP_API_KEY}`)
+        axios.get(`${process.env.REACT_APP_API_URL_HOURLY}units=${unit}&lat=${myLat}&lon=${myLong}&appid=${process.env.REACT_APP_API_KEY}`)
             .then(response => {
                 setResponseObjHourly(response.data);
                 console.log(responseObjHourly);
@@ -109,24 +118,28 @@ function Weather() {
             setError('Unavailable')
             setResponseObj({});
         }
+        setViewport({
+            width: '50vw',
+            height: '40vh',
+            zoom: 12,
+            coords: [myLat, myLong]
+        })
     }
-
-    
 
     return (
         <div className="container mx-auto pb-20">
             <div className="text-center">
                 <form onSubmit={getForecast}>
-                    <div className="flex justify-center">
+                    <div className="md:flex block justify-center">
                         <input
-                            className="mt-12 mb-4 w-1/2 bg-gray-200 py-4 px-6 text-2xl rounded-md"
+                            className="mt-12 mb-4 md:w-1/2 bg-gray-200 py-4 px-6 text-2xl rounded-md"
                             type="text"
                             placeholder="Search weather"
                             maxLength="50"
                             value={city}
                             onChange={(e) => setCity(e.target.value)}
                         /><br></br>
-                        <button className="mt-14 ml-2 p-2 h-4/5 bg-green-300 hover:bg-green-200 transition rounded-lg font-bold" type="submit"><img src={loupe} alt="search" /></button>
+                        <button className="md:mt-14 my-4 ml-2 p-2 h-4/5 bg-green-300 hover:bg-green-200 transition rounded-lg font-bold" type="submit"><img src={loupe} alt="search" /></button>
                     </div>
                     <select className="bg-gray-200 py-2 px-4" onChange={(e) => setUnit(e.target.value)}>
                         <option hidden>Select unit</option>
@@ -136,7 +149,7 @@ function Weather() {
 
 
                 </form>
-                <div className="text-left">
+                <div className="text-center md:text-left">
                     <button className="mt-4 bg-blue-300 hover:bg-blue-200 transition py-2 px-4 rounded-lg font-bold" onClick={getMyPosition}>
                         Get my position's Forecast
                 </button>
@@ -147,13 +160,13 @@ function Weather() {
                 <Forecast responseObj={responseObj} responseObjHourly={responseObjHourly} errorMessage={errorMessage} unit={unit} />
             }
 
-            {!responseObj ? null : <div className="flex justify-center mt-20 ">
-                <ReactMapGL className="border-2 border-black" longitude={long} latitude={lat} {...viewport}
+            {!responseObj ? null : <div className="flex justify-center mt-20">
+                <ReactMapGL className="border-2 border-black " longitude={long ?long : myLong} latitude={lat ? lat : myLat} {...viewport}
                     mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
                     onViewportChange={viewport => {
                         setViewport(viewport)
                     }}
-                    mapStyle="mapbox://styles/oskarb/cko00ufus570817o5ubebkcmi"><Marker mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN} longitude={long} latitude={lat}><img src={pin}/></Marker></ReactMapGL></div>}
+                    mapStyle="mapbox://styles/oskarb/cko00ufus570817o5ubebkcmi"><Marker mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN} coordinates={[34.98234, 23.1313]}><img src={pin} /></Marker></ReactMapGL></div>}
 
         </div>
     );
